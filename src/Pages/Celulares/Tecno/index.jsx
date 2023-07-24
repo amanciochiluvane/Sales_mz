57ffeb30e240348ddd719265b706876581b57d23
Amâncio Chiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2023-07-24 11:33:26 +0200

    Made the Html part for Tecno

--- src/Pages/Celulares/Tecno/index.jsx
+++ src/Pages/Celulares/Tecno/index.jsx
@@ -44,34 +44,143 @@ export default function Tecno(){
 
     return(
         <div className='Tecno'>
-             <h2>POP </h2>
-            <section className='POP'>
-            <Slider{...settings}>
-            <div id="card" className='F04'>
-                     <h1>POP 7</h1>
-                     <ul id='espec' type="none">
-                        <li>Câmera: 13MP</li>
-                        <li>Bateria: 5.000 mAh</li>
-                        <li>Espaço: 64 GB</li> 
-                        <li>RAM: 4GB</li> 
-                        <li><p className='Price'>8.600 MT</p></li>
-                        <li><p className="Comprar" ><a href="">Compre</a></p></li>
-                     </ul>
-                    </div>
+            <h2>POP</h2>
+        <section className='POP'>
+  <Slider {...settings}>
+    <div id="card" className='POP7'>
+      <h1>POP 7</h1>
+      <ul id='espec' type="none">
+        <li>Câmera: 8MP</li>
+        <li>Bateria: 5.000 mAh</li>
+        <li>Espaço: 64 GB</li> 
+        <li>RAM: 2GB</li> 
+        <li><p className='Price'>7.500 MT</p></li>
+        <li><p className="Comprar" ><a href="">Compre</a></p></li>
+      </ul>
+    </div>
 
-                    <div id="card" className='F13'>
-                     <h1>Galaxy F13</h1>
-                     <ul id='espec' type="none">
-                        <li>Câmera: 50MP</li>
-                        <li>Bateria: 6.000 mAh</li>
-                        <li>Espaço: 128 GB</li> 
-                        <li>RAM: 6GB</li> 
-                        <li><p className='Price'>11.500 MT</p></li>
-                        <li><p className="Comprar" ><a href="">Compre</a></p></li>
-                     </ul>
-                    </div>    
-            </Slider>
-            </section>
+
+    <div id="card" className='POP7P'>
+      <h1>Pop7 Pro</h1>
+      <ul id='espec' type="none">
+        <li>Câmera: 13MP</li>
+        <li>Bateria: 5.000 mAh</li>
+        <li>Espaço: 64 GB</li> 
+        <li>RAM: 3GB</li> 
+        <li><p className='Price'>8.200,00 MT</p></li>
+        <li><p className="Comprar" ><a href="">Compre</a></p></li>
+      </ul>
+    </div>
+  </Slider>
+</section>
+
+<h2>SPARK</h2>
+<section className='SPARK'>
+  <Slider {...settings}>
+    <div id="card" className='Spark10'>
+      <h1>Spark 10 </h1>
+      <ul id='espec' type="none">
+        <li>Câmera: 50MP</li>
+        <li>Bateria: 5.000 mAh</li>
+        <li>Espaço: 128 GB</li> 
+        <li>RAM: 8GB</li> 
+        <li><p className='Price'>10.500,00 MT</p></li>
+        <li><p className="Comprar" ><a href="">Compre</a></p></li>
+      </ul>
+    </div>
+
+    <div id="card" className='Spark10C'>
+      <h1>Spark 10C</h1>
+      <ul id='espec' type="none">
+        <li>Câmera: 16MP</li>
+        <li>Bateria: 5.000 mAh</li>
+        <li>Espaço: 128 GB</li> 
+        <li>RAM: 4GB</li> 
+        <li><p className='Price'>9.700,00 MT</p></li>
+        <li><p className="Comprar" ><a href="">Compre</a></p></li>
+      </ul>
+    </div>
+
+
+    <div id="card" className='Spark10P'>
+      <h1>Spark 10 pro </h1>
+      <ul id='espec' type="none">
+        <li>Câmera: 64MP</li>
+        <li>Bateria: 6.000 mAh</li>
+        <li>Espaço: 128 GB</li> 
+        <li>RAM: 6GB</li> 
+        <li><p className='Price'>12.200,00 MT</p></li>
+        <li><p className="Comprar" ><a href="">Compre</a></p></li>
+      </ul>
+    </div>
+
+  </Slider>
+</section>
+
+<section className='Camon'>
+  <Slider {...settings}>
+    <div id="card" className='Camon15'>
+      <h1>Camon 15 </h1>
+      <ul id='espec' type="none">
+        <li>Câmera: 48MP</li>
+        <li>Bateria: 5.000 mAh</li>
+        <li>Espaço: 64 GB</li> 
+        <li>RAM: 4GB</li> 
+        <li><p className='Price'>13.000,00 MT</p></li>
+        <li><p className="Comprar" ><a href="">Compre</a></p></li>
+      </ul>
+    </div>
+
+    <div id="card" className='Camon19P'>
+      <h1>Camon 19 pro</h1>
+      <ul id='espec' type="none">
+        <li>Câmera: 64MP</li>
+        <li>Bateria: 5.000 mAh</li>
+        <li>Espaço: 256 GB</li> 
+        <li>RAM: 8GB</li> 
+        <li><p className='Price'>22.300,00 MT</p></li>
+        <li><p className="Comprar" ><a href="">Compre</a></p></li>
+      </ul>
+    </div>
+
+    <div id="card" className='Camon 20'>
+      <h1>Camon 20</h1>
+      <ul id='espec' type="none">
+        <li>Câmera: 64MP</li>
+        <li>Bateria: 5.000 mAh</li>
+        <li>Espaço: 256 GB</li> 
+        <li>RAM: 8GB</li> 
+        <li><p className='Price'>15.200,00 MT</p></li>
+        <li><p className="Comprar" ><a href="">Compre</a></p></li>
+      </ul>
+    </div>
+
+    <div id="card" className='Camon20P'>
+      <h1>Camon 20 pro b</h1>
+      <ul id='espec' type="none">
+        <li>Câmera: 64MP</li>
+        <li>Bateria: 5.000 mAh</li>
+        <li>Espaço: 256 GB</li> 
+        <li>RAM: 8GB</li> 
+        <li><p className='Price'>17.500,00 MT</p></li>
+        <li><p className="Comprar" ><a href="">Compre</a></p></li>
+      </ul>
+    </div>
+
+    <div id="card" className='Camon20pre'>
+      <h1>Camon 20 premier</h1>
+      <ul id='espec' type="none">
+        <li>Câmera: 50MP</li>
+        <li>Bateria: 5.000 mAh</li>
+        <li>Espaço: 512 GB</li> 
+        <li>RAM: 8GB</li> 
+        <li><p className='Price'>28.800,00 MT</p></li>
+        <li><p className="Comprar" ><a href="">Compre</a></p></li>
+      </ul>
+    </div>
+  </Slider>
+</section>
+
         </div>
     )
 }
