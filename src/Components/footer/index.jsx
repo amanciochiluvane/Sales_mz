fa6b5a44e84ca28b84623ff3cde9dde6e0d69793
Amâncio Chiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2023-08-02 10:40:23 +0200

    Created the required files

--- src/Components/footer/index.jsx
+++ src/Components/footer/index.jsx
@@ -6,7 +6,7 @@ import sapp from '../../Assets/mobile/logo-whatsapp.png';
 export default function footer(){
     return(
        <div className="footer">
-            <h1 className="footerlogo" ><p className='sales'>Sales</p><p className='mz'>Mz</p></h1>
+            <h1 className="footerlogo" f><p className='sales'>Sales</p><p className='mz'>Mz</p></h1>
 
             
                <h2 className="Us">About Us</h2> 
@@ -19,13 +19,13 @@ export default function footer(){
                 
                 <div className='cont'>
                 <p><a href="https://wa.me/+258877109499"><img className='whatsapp' src={sapp}/></a></p>
-                <p ><a id="Links" href="mailto:herminionino380@gmail.com"><img className='email' src={email} /></a></p>
+                <p ><a id="Links" href="mailto:herminionino380@gmail.com"><img className='email' src={email} alt="" srcset="" /></a></p>
                 <p ><a id="Links" href="https://instagram.com/store_salesmz?igshid=MzRlODBiNWFlZA==" target="_blank" rel="noopener noreferrer"><img className='insta' src={insta} /></a></p>
                 </div>
                 
             
                 <h3>
-                <div>(Copyright) <h1><p className='sales'>Sales</p><p className='mz'>Mz</p></h1> - Todos os direitos reservados.</div>
+                <p>(Copyright) <h1><p className='sales'>Sales</p><p className='mz'>Mz</p></h1> - Todos os direitos reservados.</p>
                 </h3>
                 
        </div>
